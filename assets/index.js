e3815d25127f47f7c5ae7fb8f2d8371f5b2ee887
andres22 <afpinchao8@misena.edu.co>
2020-08-08 21:18:43 -0500

    cambio en distribucion del proyecto y creacion de carpeta ejercicios

---
+++ assets/index.js
@@ -0,0 +1,27 @@
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+
+
+class MediaPlayer{
+
+ constructor(config){
+
+   this.media = config.el;
+  
+
+ }
+
+ playAndPause(){
+   if(this.media.paused){
+   this.media.play();
+   }else{
+     this.media.pause();
+   }
+
+}
+}
+
+
+const player = new MediaPlayer({el:video});
+
+  button.onclick = () => player.playAndPause()
